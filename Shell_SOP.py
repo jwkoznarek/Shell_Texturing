eddcf7d10bfabc298121cbf236c08a975d0c8977
jwkoznarek <127558606+jwkoznarek@users.noreply.github.com>
2024-01-13 08:40:45 -0500

    Update Shell_SOP.py
    Added functionality to modulate between a cone and cylinder shape, changed some parameters from Int to Float, changed definition of one parameter.

--- Shell_SOP.py
+++ Shell_SOP.py
@@ -4,7 +4,7 @@ def onSetupParameters(op):
 
 	p = page.appendInt('Shellnum', label='Shell Count')[0]
 	p.min = 1
-	p.max = 10
+	p.max = 50
 	p.normMin = p.min
 	p.normMax = p.max
 	p.clampMin = True
@@ -18,7 +18,15 @@ def onSetupParameters(op):
 	p.clampMin = True
 	p.clampMax = True
 	
-	p = page.appendInt('Radius', label='Radius')[0]
+	p = page.appendFloat('Fdeg', label='Top Shell Max Size')[0]
+	p.min = 0.0
+	p.max = 1.0
+	p.normMin = p.min
+	p.normMax = p.max
+	p.clampMin = True
+	p.clampMax = True
+
+	p = page.appendFloat('Shelldist', label='Shell Distance')[0]
 	p.min = 1
 	p.max = 50
 	p.normMin = p.min
@@ -26,15 +34,7 @@ def onSetupParameters(op):
 	p.clampMin = True
 	p.clampMax = True
 
-	p = page.appendInt('Shelldist', label='Shell Distance')[0]
-	p.min = 1
-	p.max = 50
-	p.normMin = p.min
-	p.normMax = p.max
-	p.clampMin = True
-	p.clampMax = True
-
-	p = page.appendInt('Conesize', label='Cone Size')[0]
+	p = page.appendFloat('Conesize', label='Cone Size')[0]
 	p.min = 1
 	p.max = 10
 	p.normMin = p.min
@@ -42,7 +42,7 @@ def onSetupParameters(op):
 	p.clampMin = True
 	p.clampMax = True
 
-	p = page.appendInt('Distance', label='Distance')[0]
+	p = page.appendFloat('Distance', label='Distance')[0]
 	p.min = 1
 	p.max = 100
 	p.normMin = p.min
@@ -78,8 +78,8 @@ def onCook(op):
 
 	# Applies parameters to variables
 	rseg = op.par.Radial.eval()
-	radius = op.par.Radius.eval()
-	hseg = op.par.Shellnum.eval()
+	Fdeg = op.par.Fdeg.eval()
+	Shellnum = op.par.Shellnum.eval()
 	sdist = op.par.Shelldist.eval()
 	csize = op.par.Conesize.eval()
 	Densityz = op.par.Densityz.eval()
@@ -92,11 +92,11 @@ def onCook(op):
   
 	for l in range(Densityz):
 		for k in range(Densityx):
-			for i in range(hseg):
+			for i in range(Shellnum):
 				for j in range(rseg - 1, -1, -1):
 					p = op.appendPoint()
-					h = i / hseg + (radius / 10)
-					newRadius = 4 * (csize - h) # size should be normalized between 0 and 1 for additional shells
+					h = i / Shellnum
+					newRadius = (csize - (h - (Fdeg * h)))
 					p.x = math.cos(math.pi * 2 * j / rseg) * newRadius + (k * dist)
 					p.y = i * (sdist * 0.1)
 					p.z = math.sin(math.pi * 2 * j / rseg) * newRadius + (l * dist)
@@ -104,12 +104,12 @@ def onCook(op):
   # PRIMITIVE CALC
   ################
   
-	total = int(hseg * Densityx * Densityz)
+	total = int(Shellnum * Densityx * Densityz)
 	for i in range(total):
 		poly = op.appendPoly(rseg, closed=True, addPoints=False)
 		for j in range(rseg):
 			n = j + (i * rseg)
 			poly[j].point = op.points[n]
-			print('j: ' + str(j) + ' i: ' + str(i))
 		
+	print('points ' + str(i * rseg) + ' polygons: ' + str(i))
 	return
